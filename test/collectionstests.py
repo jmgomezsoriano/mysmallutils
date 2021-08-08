8eb8fb59e01739c5234bf5185072335110da9f11
José Manuel <jmgomez.soriano@gmail.com>
2021-08-08 18:53:48 +0200

    Spliting the collection tests into two tests.

--- test/collectionstests.py
+++ test/collectionstests.py
@@ -4,11 +4,13 @@ from mysutils.collections import dh, sh
 
 
 class MyTestCase(unittest.TestCase):
-    def test_dh(self):
+    def test_dh(self) -> None:
         d = {i: chr(97 + i) for i in range(26)}
         d2 = dh(d)
         self.assertEqual(len(d2), 10)
-        self.assertDictEqual(d2, {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j'})  # add assertion here
+        self.assertDictEqual(d2, {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j'})
+
+    def test_sh(self) -> None:
         s = {chr(97 + i) for i in range(26)}
         s2 = sh(s, 5)
         self.assertEqual(len(s2), 5)
