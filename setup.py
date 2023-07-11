020e0f6c59b4882ac36fcfb98207cf7bee991372
José Manuel Gómez <jose.gomez-soriano@inetum.altia.es>
2023-07-11 16:58:55 +0200

    Removing the retrocompatibility with python 3.6.

--- setup.py
+++ setup.py
@@ -42,5 +42,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6,<4'
+    python_requires='>=3.7,<4'
 )
