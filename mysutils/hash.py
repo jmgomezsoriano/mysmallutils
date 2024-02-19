6358261a96610284b6a8a64e6f60729b5d9076e5
José Manuel Gómez <jose.gomez-soriano@inetum.altia.es>
2024-02-19 17:19:27 +0100

    Adding some digest_file to support python versions < 3.11.

--- mysutils/hash.py
+++ mysutils/hash.py
@@ -3,6 +3,34 @@ from os import PathLike
 from typing import Callable, Union
 
 
+def file_digest(fileobj, digest, /, *, _bufsize=2**18):
+    """Hash the contents of a file-like object. Returns a digest object.
+
+    *fileobj* must be a file-like object opened for reading in binary mode.
+    It accepts file objects from open(), io.BytesIO(), and SocketIO objects.
+    The function may bypass Python's I/O and use the file descriptor *fileno*
+    directly.
+
+    *digest* must either be a hash algorithm name as a *str*, a hash
+    constructor, or a callable that returns a hash object.
+    """
+    # On Linux we could use AF_ALG sockets and sendfile() to archive zero-copy
+    # hashing with hardware acceleration.
+    digest = digest()
+
+    # binary file, socket.SocketIO object
+    # Note: socket I/O uses different syscalls than file I/O.
+    buf = bytearray(_bufsize)  # Reusable buffer to reduce allocations.
+    view = memoryview(buf)
+    while True:
+        size = fileobj.readinto(buf)
+        if size == 0:
+            break  # EOF
+        digest.update(view[:size])
+
+    return digest
+
+
 def file_md5(filename: Union[str, PathLike, bytes], hex: bool = True, buffer_size: int = 65536) -> (str | bytes):
     """ Generate a md5 hash from the content of a file.
 
@@ -86,5 +114,5 @@ def _hash_file(
     """
     # hash_method = method()
     with open(filename, 'rb') as file:
-        content_hash = hashlib.file_digest(file, method, _bufsize=buffer_size)
+        content_hash = file_digest(file, method, _bufsize=buffer_size)
     return content_hash.hexdigest() if hex else content_hash.digest()
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class CleanCommand(setuptools.Command):
 setuptools.setup(
     cmdclass={'clean': CleanCommand},
     name='mysmallutils',
-    version='2.0.15',
+    version='2.0.16',
     url='https://github.com/jmgomezsoriano/mysmallutils',
     license='LGPL2',
     author='José Manuel Gómez Soriano',
